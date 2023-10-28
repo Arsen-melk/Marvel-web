cc5771634d2f5c3c5daa0412e685c2571cb46638
Arsen-melk <arsenmelk001@gmail.com>
2023-10-28 15:03:55 +0400

    Development Marvel

---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import { MainPage, ComicsPage}  from "../pages";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppHeader from "../appHeader/AppHeader";
+
+
+
+const App = () => {
+
+    
+    return(
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+           </div>
+
+        </Router>
+    )
+}
+
+export default App;
